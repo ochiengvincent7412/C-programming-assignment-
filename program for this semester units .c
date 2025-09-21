3f036040f294c1045c9ff40c35b991ce457ae1f9
ochiengvincent7412 <ochiengvincent7412@gmail.com>
2025-09-21 14:32:27 +0300

    Add files via upload

---
+++ program for this semester units .c
@@ -0,0 +1,20 @@
+/*
+Name;Owigo Vincent Ochieng 
+Reg number;PA106/G/28807/25
+Description; program to display this semester units 
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("1.Discrete mathematics (SPM 2100)\n");
+    printf("2.Mathematics for science (SPM 2104)\n");
+    printf("3.Fundamentals is ICT (UCU 2105)\n");
+    printf("4.Computer system and organisation (SPI 2102)\n");
+    printf("5.Itroduxtion to programming and problem solving (SPC 2101)\n");
+    printf("6.Fundamentals of system analysis and design (SSE 2101)\n");
+    printf("7.Communication skills (UCU 2101)\n");
+    
+    return 0;
+}
