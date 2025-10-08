4b40322219f3f6d8c45faf9bca8a2919fec27cb0
ochiengvincent7412 <ochiengvincent7412@gmail.com>
2025-10-08 09:42:32 +0300

    Add files via upload

---
+++ for_loop_100to50.c
@@ -0,0 +1,18 @@
+/*
+Name;Vincent Ochieng Owigo 
+Reg number;PA106/G/28807/25
+Description;for loop program to print numbers down from 100 to 50
+*/
+
+#include<stdio.h>
+
+int main()
+{
+  int i;
+  
+  for(int i=100; i>=50; i--)
+  {
+  printf("%d\n",i);
+  }
+    return 0;
+}
